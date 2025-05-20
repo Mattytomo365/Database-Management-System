91fd0dc77d7305cb866cbf3dd69c6efe0eb77122
Mattytomo365 <matty.tom@icloud.com>
2025-05-20 18:53:09 +0100

    Initialisation of logic and GUI

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
 import tkinter as tk
+
+root = tk.Tk()
+root.title("Volunteer Management System")
+root.geometry("800x600")
+
+root.loop()
+
--- logic.py
+++ logic.py
@@ -1,1 +1,21 @@
 import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor() # A mechanism that enables traversal of records in a database
+
+def create_table():
+    pass
+
+def add_volunteer():
+    pass
+
+def edit_volunteer():
+    pass
+
+def delete_volunteer():
+    pass
+
+def retrive_volunteer():
+    pass
+
+
