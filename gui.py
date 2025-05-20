e393f895c7d077d645333655988b9e0359bf7d52
Mattytomo365 <matty.tom@icloud.com>
2025-05-20 18:55:37 +0100

    Adjustements to GUI

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Volunteer Management System")
 root.geometry("800x600")
+root.configure(bg="white")
+root.resizable(False, False)
 
-root.loop()
+root.mainloop()
 
