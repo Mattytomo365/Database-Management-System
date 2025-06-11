fa18c28998c723119d8a8730e9374c6e398bec6b
Mattytomo365 <matty.tom@icloud.com>
2025-06-11 12:21:15 +0100

    Checking and handling of delete institution eligibility

--- gui.py
+++ gui.py
@@ -775,7 +775,7 @@ def delete_volunteer_popup():
     delete_volunteer_popup.configure(bg="white")
     delete_volunteer_popup.resizable(False, False)
     header = tk.Label(delete_volunteer_popup, text= "Delete Volunteer", font=('Arial', 30), bg="white", fg="dark blue")
-    header.grid(row=0, column=0, padx=120, pady=10, columnspan=2)
+    header.grid(row=0, column=0, padx=90, pady=10, columnspan=2)
 
     volunteer_names = get_volunteer_names()
 
@@ -796,27 +796,44 @@ def delete_volunteer_popup():
 def delete_institution_popup():
     delete_institution_popup = tk.Toplevel(root)
     delete_institution_popup.title("Delete Institution")
-    delete_institution_popup.geometry("470x200")
+    delete_institution_popup.geometry("470x250")
     delete_institution_popup.configure(bg="white")
     delete_institution_popup.resizable(False, False)
+
+    def delete_institution_popup_specific(selected):
+        nonlocal delete_institution_frame
+        delete_institution_frame.destroy()
+        delete_institution_frame = tk.Frame(delete_institution_popup, bg='white')
+        delete_institution_frame.grid(row=2, column=0, pady=10, columnspan=2)
+
+        if delete_institution_eligibility(institution_dropdown.get()):
+            delete_institution_button = ttk.Button(delete_institution_frame, text="Delete", style="Blue.TButton", command=lambda: [delete_institution(institution_dropdown.get()), delete_institution_popup.destroy()])
+            delete_institution_button.grid(row=0, column=0, pady=10, columnspan=2)
+        else:
+            warning_label = tk.Label(delete_institution_frame, text='Volunteers in the system are linked to this institution, unable to delete', font=('Arial', 10), bg="white", fg="red" )
+            warning_label.grid(row=0, column=0, pady=10, columnspan=2)
+            delete_institution_button = ttk.Button(delete_institution_frame, text="Delete", style="Blue.TButton", state='disabled')
+            delete_institution_button.grid(row=1, column=0, pady=10, columnspan=2)
+
     header = tk.Label(delete_institution_popup, text= "Delete Institution", font=('Arial', 30), bg="white", fg="dark blue")
-    header.place(x=240, y=30, anchor=tk.CENTER)
+    header.grid(row=0, column=0, padx=90, pady=10, columnspan=2)
 
     institution_names = get_institution_names()
 
-    if institution_names:
-        dropdown_label = tk.Label(delete_institution_popup, text="Institution", font=('Arial', 15), bg="white", fg="black")
-        dropdown_label.place(x=100, y=100, anchor=tk.CENTER)
-        institution_var = tk.StringVar(delete_institution_popup)
-        institution_chosen = ttk.Combobox(delete_institution_popup, width=34, textvariable=institution_var)
-        institution_chosen['values'] = institution_names
-        institution_chosen.place(x=300, y=100, anchor=tk.CENTER)
+    delete_institution_frame = tk.Frame(delete_institution_popup, bg='white')
+    delete_institution_frame.grid(row=2, column=0, pady=10, columnspan=2)
 
-        delete_institution_button = ttk.Button(delete_institution_popup, text="Delete", style="Blue.TButton", command=lambda: [delete_institution(), delete_institution_popup.destroy()])
-        delete_institution_button.place(x=230, y=170, anchor=tk.CENTER)
+    if institution_names:
+        institution_dropdown_label = tk.Label(delete_institution_popup, text="Institution", font=('Arial', 15), bg="white", fg="black")
+        institution_dropdown_label.grid(row=1, column=0, pady=10)
+        institution_var = tk.StringVar(delete_institution_popup)
+        institution_dropdown = ttk.Combobox(delete_institution_popup, width=34, textvariable=institution_var)
+        institution_dropdown['values'] = institution_names
+        institution_dropdown.grid(row=1, column=1, sticky='w', pady=10)
+        institution_dropdown.bind("<<ComboboxSelected>>", delete_institution_popup_specific)
     else:
         no_institutions_label = tk.Label(delete_institution_popup, text="No institutions to delete", font=('Arial', 15), bg="white", fg="black")
-        no_institutions_label.place(x=230, y=100, anchor=tk.CENTER)
+        no_institutions_label.grid(row=1, column=0, pady=10, columnspan=2)
 
 def delete_role_popup():
     delete_role_popup = tk.Toplevel(root)
